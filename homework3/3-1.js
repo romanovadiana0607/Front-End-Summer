45552068b0314b16b44aca6638023d4a30eebcca
Diana Romanova <romanovadi0607@gmail.com>
2024-05-22 20:39:05 -0400

    3.1

---
+++ homework3/3-1.js
@@ -0,0 +1,42 @@
+// Assignment 3.1
+// Diana Romanova 
+
+
+//number
+let num = 5;
+
+//boolean
+let bool = true;
+
+
+//undefined
+let undef;
+
+//string
+let str = "hello";
+
+//Bigint
+let bigInt = 723428648764827474627642648742947;
+
+//null
+let a = null;
+
+//object
+const car = {
+    color: "black",
+    price: 50000
+};
+
+//array
+let arr = [1,2,3,4];
+
+
+
+console.log( typeof(num)); 
+console.log(typeof(bool));
+console.log( typeof(undef));
+console.log( typeof(str));
+console.log( typeof(bigInt));
+console.log( typeof(a));
+console.log( typeof(car));
+console.log( typeof(arr));
