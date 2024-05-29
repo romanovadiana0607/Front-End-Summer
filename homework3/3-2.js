0ddc85f8c9e821471fb3470416084014f04c5896
Diana Romanova <romanovadi0607@gmail.com>
2024-05-29 14:58:59 -0400

    3.2

--- homework3/3-2.js
+++ homework3/3-2.js
@@ -1,8 +1,8 @@
 // Diana Romanova 
 //Assignment 3.2
 
-let str1 = prompt("Enter the first string");
-let str2 = prompt("Enter the second string");
-let str3 = prompt("Enter the third string");
+// let str1 = prompt("Enter the first string");
+// let str2 = prompt("Enter the second string");
+// let str3 = prompt("Enter the third string");
 
-console.log(str1, str2, str3);
+// console.log(str1, str2, str3);
